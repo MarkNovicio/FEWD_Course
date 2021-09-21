a1b336f8799d5505b6992fd4aab23d70d0a27074
marknovicio <marknovicio@gmail.com>
2021-09-20 19:25:13 -0700

    updated JS hover

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,43 @@
-function increaseTitleSize(){
-    let element = document.querySelector(".fewd-content");
-    let title = document.querySelector(".content-title")
-    element.onmouseover=function(){
-        title.css
-    }
-}
 
+const mouseTargetEnter = document.querySelector('.fewd-content p')
+mouseTargetEnter.addEventListener('mouseenter', e => {
+    mouseTargetEnter.style.fontSize = '1.2em';
+  });
 
+  const mouseTargetExit = document.querySelector('.fewd-content p')
+  mouseTargetExit.addEventListener('mouseleave', e => {
+    mouseTargetExit.style.fontSize = '1.0em';
+    });
+/*
 function increaseTitleSize() {
 	window.addEventListener(
-	  "click",
+	  "mouseenter",
 	  function (event) {
 		  
 		if (event.target.classList.contains("fewd-content")) {
-			console.log("worked")
+			
 			//console.log(document.getElementsByClassName("signin-messages-container"))
-            
-		 // document.getElementsByClassName("signin-messages-container")[0].style.display = "none"  //getElementsByClassName("signin-messages-container")  
+
+		    document.getElementsByClassName("content")[0].style.fontSize = "x-large" //getElementsByClassName("signin-messages-container")  
 		  //delivers a nodeList.
 		}
 		  this.console.log("selectedElement works!!")})}
 
-selectedElement()
+          increaseTitleSize()
+
+          function decreaseTitleSize() {
+            window.addEventListener(
+              "mouseleave",
+              function (event) {
+                  
+                if (event.target.classList.contains("fewd-content")) {
+                    
+                    //console.log(document.getElementsByClassName("signin-messages-container"))
+        
+                    document.getElementsByClassName("content")[0].style.fontSize = "normal"  //getElementsByClassName("signin-messages-container")  
+                  //delivers a nodeList.
+                }
+                  this.console.log("mouse out works!!")})}
+                     
+          decreaseTitleSize()
+         */
