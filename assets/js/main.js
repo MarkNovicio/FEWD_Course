5ab20c81c5d6cc02601bfe318995fa90fd60a47e
marknovicio <marknovicio@gmail.com>
2021-09-20 17:13:25 -0700

    add event listener click

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+function increaseTitleSize(){
+    let element = document.querySelector(".fewd-content");
+    let title = document.querySelector(".content-title")
+    element.onmouseover=function(){
+        title.css
+    }
+}
+
+
+function increaseTitleSize() {
+	window.addEventListener(
+	  "click",
+	  function (event) {
+		  
+		if (event.target.classList.contains("fewd-content")) {
+			console.log("worked")
+			//console.log(document.getElementsByClassName("signin-messages-container"))
+            
+		 // document.getElementsByClassName("signin-messages-container")[0].style.display = "none"  //getElementsByClassName("signin-messages-container")  
+		  //delivers a nodeList.
+		}
+		  this.console.log("selectedElement works!!")})}
+
+selectedElement()
